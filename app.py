d3e18a68ba2ca581d0494e0eb10fdccaaad1a513
nirpr <nirpr@mta.ac.il>
2023-10-31 13:36:20 +0200

    added features: choosing 3pt/mid and short/long

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ def index():
 
 @app.route('/process', methods=['POST'])
 def process_form():
-    out_of, right_corner, right_wing, top, left_wing, left_corner = process_data()
+    out_of, right_corner, right_wing, top, left_wing, left_corner, right_corner_type, right_wing_type, \
+        top_type, left_wing_type, left_corner_type = process_data()
     shooting_percentages = [right_corner, right_wing, top, left_wing, left_corner]
     shooting_percentages = [(int(item) / int(out_of)) * 100 for item in shooting_percentages]
     chart_filename = create_shooting_chart(shooting_percentages)
@@ -28,7 +29,15 @@ def process_data():
     top = request.form.get('Top')
     left_wing = request.form.get('LeftWing')
     left_corner = request.form.get('LeftCorner')
-    return out_of, right_corner, right_wing, top, left_wing, left_corner
+
+    right_corner_type = request.form.get('RightCornerDistance')
+    right_wing_type = request.form.get('RightWingDistance')
+    top_type = request.form.get('TopDistance')
+    left_wing_type = request.form.get('LeftWingDistance')
+    left_corner_type = request.form.get('LeftCornerDistance')
+
+    return out_of, right_corner, right_wing, top, left_wing, left_corner, right_corner_type, right_wing_type, \
+        top_type, left_wing_type, left_corner_type
 
 
 def create_shooting_chart(shooting_percentages):
